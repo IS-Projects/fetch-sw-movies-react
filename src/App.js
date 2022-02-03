20a83e75002ebd616f8140a211fd5a5a00749454
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-02 21:30:37 -0700

    edited response to match props names

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ function App() {
   const [movies, setMovies] = useState([]);
 
   const fetchMoviesHandler = async () => {
-    const response = await fetch("https://swapi.dev/api.films/").then((res) => {
+    const response = await fetch("https://swapi.dev/api/films/").then((res) => {
       if (res.ok) {
         return res.json();
       } else {
         // eslint-disable-next-line no-throw-literal
-        throw { name: "servicesError", message: await res.json() };
+        throw { name: "servicesError", message: res.json() };
       }
     });
     const results = response.results;
-    setMovies(results);
+    const transformedResults = results.map((result) => {
+      return {
+        id: result.episode_id,
+        title: result.title,
+        releaseDate: result.release_date,
+        openingText: result.opening_crawl,
+      };
+    });
+    setMovies(transformedResults);
   };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
         <MoviesList movies={movies} />
