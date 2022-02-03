beed0421170c45c28ab186b69d0cb1f404e5f531
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-02 21:58:54 -0700

    added error handling

--- src/App.js
+++ src/App.js
@@ -6,27 +6,44 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/").then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        // eslint-disable-next-line no-throw-literal
-        throw { name: "servicesError", message: res.json() };
+    setError(null);
+    try {
+      // What is commented out is the way I coded it
+      // const response = await fetch("https://swapi.dev/api/fils/").then(
+      //   async (res) => {
+      //     if (res.ok) {
+      //       return res.json();
+      //     } else {
+      //       // eslint-disable-next-line no-throw-literal
+      //       throw { name: "servicesError", message: "Something went wrong!" };
+      //     }
+      //   }
+      // );
+
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
       }
-    });
-    const results = response.results;
-    const transformedResults = results.map((result) => {
-      return {
-        id: result.episode_id,
-        title: result.title,
-        releaseDate: result.release_date,
-        openingText: result.opening_crawl,
-      };
-    });
-    setMovies(transformedResults);
+
+      const data = await response.json();
+
+      const results = data.results;
+      const transformedResults = results.map((result) => {
+        return {
+          id: result.episode_id,
+          title: result.title,
+          releaseDate: result.release_date,
+          openingText: result.opening_crawl,
+        };
+      });
+      setMovies(transformedResults);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
 
@@ -37,8 +54,11 @@ function App() {
       </section>
       <section>
         {isLoading && <p>Loading ...</p>}
-        {!isLoading && movies.length === 0 && <p>We found no movies. :(</p>}
+        {!isLoading && movies.length === 0 && !error && (
+          <p>We found no movies. :(</p>
+        )}
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
